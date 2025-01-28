756c317150723c618060949457c6f8145ab2aff9
MarkII <reyiczd@gmail.com>
2025-01-28 23:03:55 +0100

    creacion simple de un djangotemplate

--- newproject/blogpost/views.py
+++ newproject/blogpost/views.py
@@ -1,11 +1,24 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic.base import View
+from django.shortcuts import render
 
 # Create your views here.
 
 
 class HelloWorld(View):
     def get(self, request):
-        return HttpResponse(content="Hola mundo")
+        # return HttpResponse(content="Hola mundo")
+        # Creacion de Django Template:
+        
+        data = {
+            'name':'el pishita',
+            'surname':'53',
+            'languaje': ['python','c++','c','java'],
+        }
+        
+        return render(request,'creacionhtml.html',context=data, 
+        )
+
+
 
