4ceb61062751e04501ef31f656d7330071dae9b9
MarkII <reyiczd@gmail.com>
2025-01-28 22:49:09 +0100

    Creacion de una vista chorra el clasico hola mundo

--- newproject/blogpost/views.py
+++ newproject/blogpost/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic.base import View
 
 # Create your views here.
+
+
+class HelloWorld(View):
+    def get(self, request):
+        return HttpResponse(content="Hola mundo")
+
--- newproject/newproject/urls.py
+++ newproject/newproject/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from blogpost.views import HelloWorld
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',HelloWorld.as_view()),
 ]
