20f7108c68bfbc068b829d796179eaf01128994e
MarkII <reyiczd@gmail.com>
2025-01-28 23:14:42 +0100

    creacion del primer modelo, tabla en bd y migraciones

--- newproject/blogpost/models.py
+++ newproject/blogpost/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(verbose_name="Titulo", max_length=256)
+    description = models.TextField(verbose_name="Descripción")
+    created_at = models.DateTimeField(auto_created=True)
+    
