1642b8436cd4f74d95783cb9710fd018fb51eaeb
MarkII <reyiczd@gmail.com>
2025-01-29 18:11:45 +0100

    Creeacion dee objeetos nuevos dentro del adminpanel

--- newproject/blogpost/admin.py
+++ newproject/blogpost/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from blogpost.models import Post
+
+
+# decorador para tener un form de creacion een el panel de administración.
+
+@admin.register(Post)
+class AdminPost(admin.ModelAdmin):
+    
+    # propiedades del modelo que queremos que muestre en el panel de admin.
+    
+    list_display = ['title', 'description', 'created_at']
+    
