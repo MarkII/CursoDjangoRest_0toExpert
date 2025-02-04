0ce748c494e6b3e8d397eef9f40457461e4fa073
MarkII <reyiczd@gmail.com>
2025-02-04 20:42:16 +0100

    peersonalizzacionn deel user nuevo en el admin

--- newproject/user/admin.py
+++ newproject/user/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin as BaseUserAdminX
+from user.models import User
 # Register your models here.
+
+@admin.register(User)
+class UserAdmin(BaseUserAdminX):
+    fieldsets = (
+        (None, {'fields':('username', 'password')}),
+        ('Info User', {'fields':('first_name', 'last_name', 'email')}),
+        ('Otros', {'fields':('is_active',)})
+    )
