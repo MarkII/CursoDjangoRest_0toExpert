d1bc578d4def8ddf3d593c027219bf237b0dd425
MarkII <reyiczd@gmail.com>
2025-02-04 23:52:12 +0100

    cambiamos los datos que se usan para iniciar sesion

--- newproject/user/models.py
+++ newproject/user/models.py
@@ -4,8 +4,11 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
+    email = models.EmailField(unique=True)
     web_site = models.CharField(max_length=256, blank=True)
     twitter = models.CharField(max_length=128, blank=True)
     
+    USERNAME_FIELD = 'email' # Cambiando el identificador useername por email
+    REQUIRED_FIELDS = []
     
     
