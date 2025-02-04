e340a06f94595954bdcd7d6f688e57d6e3fccc83
MarkII <reyiczd@gmail.com>
2025-02-04 20:18:39 +0100

    installacion del drs_yag y configuracion

--- newproject/newproject/settings.py
+++ newproject/newproject/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blogpost',
     'rest_framework',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
@@ -118,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = './static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
