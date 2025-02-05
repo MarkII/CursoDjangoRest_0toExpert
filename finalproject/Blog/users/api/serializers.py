af5d4ebedb9aafa372c614112f2f24dc4dbe94bc
MarkII <reyiczd@gmail.com>
2025-02-05 23:13:43 +0100

    eencriptacion de password

--- finalproject/Blog/users/api/serializers.py
+++ finalproject/Blog/users/api/serializers.py
@@ -2,6 +2,17 @@ from rest_framework.serializers import ModelSerializer
 from users.models import  User as BlogUser
 
 class UserRegisterSerializer(ModelSerializer):
+    
     class Meta:
         model = BlogUser
         fields = ['id', 'email', 'username', 'password']
+        
+    def create(self, validated_data):
+        password =  validated_data.pop('password', None)
+        instance = self.Meta.model(**validated_data)
+       
+        if password is not None:
+           instance.set_password(password)
+        instance.save()
+        return instance
+        
