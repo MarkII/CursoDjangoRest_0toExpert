971c03a1c8f6984723b92c051b6dcd7de38b90b6
MarkII <reyiczd@gmail.com>
2025-02-05 20:52:42 +0100

    cleaning all

--- newproject/blogpost/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Post(models.Model):
-    title = models.CharField(verbose_name="Titulo", max_length=256)
-    description = models.TextField(verbose_name="Descripción")
-    created_at = models.DateTimeField(auto_created=True)
-    
