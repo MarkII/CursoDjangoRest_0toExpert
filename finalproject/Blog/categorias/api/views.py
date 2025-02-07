1cd72b99a5ff4ab7b05e3919d2d281b8847ac7a5
MarkII <reyiczd@gmail.com>
2025-02-07 21:53:57 +0100

    creando app y rutas de categoria

---
+++ finalproject/Blog/categorias/api/views.py
@@ -0,0 +1,12 @@
+from categorias.models import Categorias
+from categorias.api.serializers import CategoriasSerializer
+
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+
+class CategoriaModelViewSet(ModelViewSet):
+    queryset = Categorias.objects.all()
+    serializer_class  = CategoriasSerializer
+    
