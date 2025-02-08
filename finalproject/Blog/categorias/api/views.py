2e510bffabe960b8ab458fa02e58ffb9468bec2d
MarkII <reyiczd@gmail.com>
2025-02-08 17:45:32 +0100

    Creacion de peermiso para categoria, isadminreadonnly

--- finalproject/Blog/categorias/api/views.py
+++ finalproject/Blog/categorias/api/views.py
@@ -1,5 +1,6 @@
 from categorias.models import Categorias
 from categorias.api.serializers import CategoriasSerializer
+from categorias.api.permissions import IsAdminReadOnly
 
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
@@ -9,4 +10,5 @@ from rest_framework.permissions import IsAuthenticated, IsAdminUser
 class CategoriaModelViewSet(ModelViewSet):
     queryset = Categorias.objects.all()
     serializer_class  = CategoriasSerializer
+    permission_classes = [IsAdminReadOnly]
     
